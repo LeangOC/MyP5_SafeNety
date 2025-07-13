41fae9384ee2cadb34334274a02b171db483acc3
Leang Chouteau <chouteau.leang@gmail.com>
2025-07-13 21:39:57 +0200

    My First API Rest /hello

--- src/main/java/oc/p5/SafeNety/SafeNetyApplication.java
+++ src/main/java/oc/p5/SafeNety/SafeNetyApplication.java
@@ -2,12 +2,18 @@ package oc.p5.SafeNety;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SafeNetyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SafeNetyApplication.class, args);
 	}
-
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }

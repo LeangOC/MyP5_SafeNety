da89e60a7c6d77e4e285cfe2f1600343eeb2649e
Leang Chouteau <chouteau.leang@gmail.com>
2025-07-16 21:54:39 +0200

    Mise en place de logger requête HTTP GET request to 'firestation?stationNumber=2

---
+++ src/main/java/oc/p5/SafeNety/filter/RequestLoggingFilter.java
@@ -0,0 +1,35 @@
+package oc.p5.SafeNety.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class RequestLoggingFilter implements Filter {
+
+    private static final Logger logger = LogManager.getLogger(RequestLoggingFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        String method = req.getMethod();
+        String uri = req.getRequestURI();
+        String queryString = req.getQueryString(); // contient stationNumber=1
+        String fullUrl = (queryString != null) ? uri + "?" + queryString : uri;
+
+        logger.info("HTTP {} request to '{}' from IP: {}", method, fullUrl, req.getRemoteAddr());
+
+        chain.doFilter(request, response);
+    }
+}

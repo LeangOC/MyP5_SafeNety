2ea94ba64cc062353b689e2afc9a828c50e25348
Leang Chouteau <chouteau.leang@gmail.com>
2025-07-21 19:14:45 +0200

    Suppression de DTO des getters et setter

--- src/main/java/oc/p5/SafeNety/service/FirestationService.java
+++ src/main/java/oc/p5/SafeNety/service/FirestationService.java
@@ -14,13 +14,13 @@ public class FirestationService {
 
     public void addFirestation(FirestationDTO dto) {
         Firestation f = new Firestation();
-        f.setAddress(dto.getAddress());
-        f.setStation(dto.getStation());
+        f.setAddress(dto.address);
+        f.setStation(dto.station);
         repository.save(f);
     }
 
     public void updateFirestation(FirestationDTO dto) {
-        repository.update(new Firestation(dto.getAddress(), dto.getStation()));
+        repository.update(new Firestation(dto.address, dto.station));
     }
 
     public void deleteByAddress(String address) {
